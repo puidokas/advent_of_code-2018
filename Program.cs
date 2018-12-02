f83d7ef16d62fb699c169ea2676d8b0996e0ba54
Jonas Hoffmann <jonashoffmann@outlook.dk>
2018-12-02 14:11:42 +0100

    initial commit

---
+++ Program.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace advent_of_code_2018
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Advent of code 2018 by Jonas Hoffmann");
+
+            var daysToSolve = Enumerable.Range(1, 2);
+
+            foreach (int day in daysToSolve)
+            {
+                solve(day);
+            }
+        }
+
+        private static void solve(int dayNo)
+        {
+            Console.Write("\nDay {0:D} Solution: ", dayNo);
+
+            switch (dayNo)
+            {
+                case 1:
+                    Console.WriteLine(getResultDay1());
+                    break;
+                case 2:
+                    Console.WriteLine(getResultDay2());
+                    break;
+                default:
+                    Console.WriteLine("Default case");
+                    break;
+            }
+        }
+
+        private static string[] getInput(string fileName)
+        {
+            string[] lines = System.IO.File.ReadAllLines(@"..\..\..\resources\" + fileName + ".txt");
+            return lines;
+        }
+
+        private static int getResultDay1()
+        {
+            string[] lines = getInput("input1");
+            int num, result = 0;
+            foreach (string line in lines)
+            {
+                num = Int32.Parse(line);
+                result += num;
+            }
+
+            return result;
+        }
+
+        private static int getResultDay2()
+        {
+            string[] lines = getInput("input2");
+
+            int twice = 0, thrice = 0;
+            bool twiceThisLine = false, thriceThisLine = false;
+
+            foreach (string line in lines)
+            {
+                twiceThisLine = false;
+                thriceThisLine = false;
+
+                foreach (char char_ in line)
+                {
+                    int charNumberOfTimesAppears = line.Count(x => x == char_);
+
+                    if (charNumberOfTimesAppears == 2 && !twiceThisLine)
+                    {
+                        twiceThisLine = true;
+                        twice++;
+                    }
+                    else if (charNumberOfTimesAppears == 3 && !thriceThisLine)
+                    {
+                        thriceThisLine = true;
+                        thrice++;
+                    }
+                }
+            }
+
+            int result = twice * thrice;
+
+            return result;
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,5 @@
+﻿# Advent of Code 2018
+
+"Advent of Code is an Advent calendar of small programming puzzles for a variety of skill sets and skill levels that can be solved in any programming language you like. People use them as a speed contest, interview prep, company training, university coursework, practice problems, or to challenge each other." - [AOC 2018](https://adventofcode.com/2018/about)
+
+These are my solutions to the problems presented in Advent of Code 2018. The solutions were developed in C#.
